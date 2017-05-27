f77b26dcca31001d20b9dc2f1f28f2107d4f4cf2
Gearanvil <grzebieluch.pawel@gmail.com>
2017-05-27 13:28:17 +0200

    added email validation, password validation, connection check

--- app/src/main/java/com/sdaacademy/grzebieluch/pawel/myapplication_27_05/Utils.java
+++ app/src/main/java/com/sdaacademy/grzebieluch/pawel/myapplication_27_05/Utils.java
@@ -3,6 +3,7 @@ package com.sdaacademy.grzebieluch.pawel.myapplication_27_05;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.widget.Toast;
 
 /**
  * Created by RENT on 2017-05-27.
@@ -10,24 +11,31 @@ import android.net.NetworkInfo;
 
 public class Utils {
 
+    //metoda showToast wyswietla toasta w szybkim tepie
+    public static void showToast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
     Context mContext;
+
     public static boolean isEmeailValid(String email) {
         return email.contains("@");
 
 
     }
+
     //sprawdzamy czy uzytkownik przy hasle podał ponad 6 znaków
     public static boolean isPasswordValid(String password) {
         return password.length() > 6;
     }
+
     //sprawdzanie czy posiadamy dostep do internetu
-    static boolean checkInternetConnection(Context context) {
+    public static boolean checkInternetConnection(Context context) {
         ConnectivityManager cm =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
-
 
 
 }
--- app/src/main/java/com/sdaacademy/grzebieluch/pawel/myapplication_27_05/presenters/RegisterPresenter.java
+++ app/src/main/java/com/sdaacademy/grzebieluch/pawel/myapplication_27_05/presenters/RegisterPresenter.java
@@ -1,13 +1,17 @@
 package com.sdaacademy.grzebieluch.pawel.myapplication_27_05.presenters;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.sdaacademy.grzebieluch.pawel.myapplication_27_05.Utils;
+
 
 /**
  * Created by RENT on 2017-05-27.
@@ -16,6 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class RegisterPresenter {
 
     private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
 
     public RegisterPresenter() {
@@ -24,15 +29,42 @@ public class RegisterPresenter {
 
 
     public void registerUser(final Activity activity, String email, String password) {
+        progressDialog = new ProgressDialog(activity);
 
+
+        if (TextUtils.isEmpty(email)) {
+            Utils.showToast(activity, "Enter email...");
+            return;
+        } else if (!Utils.isEmeailValid(email)) {
+            Utils.showToast(activity, "Enter the valid email...");
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)){
+            Utils.showToast(activity,"Enter password...");
+            return;
+        }else if (!Utils.isPasswordValid(password)) {
+            Utils.showToast(activity, "Password is to short");
+            return;
+        }
+
+        if(!Utils.checkInternetConnection(activity)){
+            Utils.showToast(activity,"No internet connection...");
+            return;
+        }
+
+        progressDialog.setMessage("Registration...");
+        progressDialog.show();
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(activity,
                 new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            progressDialog.dismiss();
                             Toast.makeText(activity, "Account Created", Toast.LENGTH_SHORT).show();
                         } else {
+                            progressDialog.dismiss();
                             Toast.makeText(activity, "!!!Error!!! something went wrong! !!!Error!!!", Toast.LENGTH_SHORT).show();
                         }
                     }
